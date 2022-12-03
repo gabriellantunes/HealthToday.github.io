e967991628ae805b94229ff3a89ba27cac137a64
Gabriel Antunes <106894238+gabriellantunes@users.noreply.github.com>
2022-12-03 00:53:52 -0300

    Add files via upload

---
+++ Html/index.js
@@ -0,0 +1,14 @@
+// 1º Irei criar duas constantes //
+const botaoMenu = document.querySelector(".img2") //icone do menu normalmente
+const botaoMenu2 = document.querySelector(".img3")
+const menu = document.querySelector(".menu-lateral") //nome do menu da nav normalmente
+
+
+// Iremos adicionar um evento para esse botao menu //
+botaoMenu.addEventListener("click",() => {
+    menu.classList.toggle("menu-lateral--ativo")
+})
+
+botaoMenu2.addEventListener("click",() => {
+    menu.classList.toggle("menu-lateral--ativo")
+})
